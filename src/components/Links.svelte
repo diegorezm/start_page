885205f180de0ab958b2ad0db942f7858c6faa99
diegorezm <dmourarezende@gmail.com>
2023-11-19 20:10:54 -0300

    Links component added

---
+++ src/components/Links.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Links } from "../interfaces/Links";
+  export let links: Links[];
+</script>
+
+<ul class="list">
+{#each links as link}
+  <li class="link__wrapper">
+    <h1>
+      <i class={`nf ${link.icon}`}/>
+      <a href={link.link} target="_blank">{link.title}</a>
+    </h1>
+  </li>    
+{/each}
+</ul>
