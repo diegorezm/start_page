bc788e10063f550cc94774f21bb0f07eca6ed4f4
diegorezm <dmourarezende@gmail.com>
2023-11-20 12:09:25 -0300

    clock component

---
+++ src/components/Clock.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { onMount } from "svelte";
+
+  const getCurrentTime = () => {
+    const date = new Date();
+    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  };
+
+  let currentTime = getCurrentTime();
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      currentTime = getCurrentTime();
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+<div class="clock__wrapper">
+  <i class="nf nf-oct-clock" />
+  <span>
+    {currentTime}
+  </span>
+</div>
+
+<style>
+  .clock__wrapper {
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    top:1em;
+    left: 0;
+    right: 0;
+    text-align: center;
+    font-size: 1.5rem;
+  }
+</style>
