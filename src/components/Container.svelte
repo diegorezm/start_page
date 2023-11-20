a02f8bdc893d101eee503da94c0db3edec92ff0d
diegorezm <dmourarezende@gmail.com>
2023-11-20 12:20:54 -0300

    mascara container

--- src/components/Container.svelte
+++ src/components/Container.svelte
@@ -1,9 +1,10 @@
 <div class="container">
-  <slot></slot>
+  <div class="hidden__container" />
+  <slot />
 </div>
 
 <style>
-  .container{
+  .container {
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -11,5 +12,15 @@
     width: 100%;
     height: 100%;
     position: relative;
-}
+    background-image: url("https://w.wallhaven.cc/full/1p/wallhaven-1poo61.jpg");
+  }
+  .hidden__container {
+    position: absolute;
+    background: rgba(0, 0, 0, 0.6);
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: blur(4px);
+  }
 </style>
