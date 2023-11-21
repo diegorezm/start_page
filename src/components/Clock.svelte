e757a65b4fb22641e34bfef9862766ee2098e06b
diegorezm <dmourarezende@gmail.com>
2023-11-21 14:28:13 -0300

    trying to solve build problem with navigator

--- src/components/Clock.svelte
+++ src/components/Clock.svelte
@@ -2,9 +2,15 @@
   import { onMount } from "svelte";
 
   const getCurrentTime = () => {
-    const lang = navigator.language || "pt-BR"
+    const lang = () => {
+      if (typeof navigator !== "undefined") {
+        return navigator.language;
+      } else {
+        return "pt-BR";
+      }
+    };
     const date = new Date();
-    return date.toLocaleTimeString(lang, {
+    return date.toLocaleTimeString(lang(), {
       hour: "2-digit",
       minute: "2-digit",
     });
