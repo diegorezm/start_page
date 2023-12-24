8586744d09c008f69a72b08671815d2f86380447
diegorezm <dmourarezende@gmail.com>
2023-12-24 15:53:51 -0300

    organization

--- src/lib/interfaces/index.ts
+++ src/lib/interfaces/index.ts
@@ -1,13 +1,13 @@
 import type { Writable } from "svelte/store";
 
-export enum Tag {
-  mydia = "mydia",
-  com = "com"
-}
-
 export interface Wallpaper {
   wallpaper: Writable<string>,
   setWallpaper: (url: string) => void
+}
+
+export enum Tag {
+  mydia = "mydia",
+  com = "com"
 }
 
 export interface Links {
