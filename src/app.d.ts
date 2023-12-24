dc0f70278a71c2163ebd820e4739f300fed4b3f5
diegorezm <dmourarezende@gmail.com>
2023-12-24 15:52:53 -0300

    page data type

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { Bookmarks } from "$lib/interfaces";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+      data: Bookmarks
+    }
 		// interface Platform {}
 	}
 }
