dc561f2140259dcdd4067c3ab1c42d376c11a163
diegorezm <dmourarezende@gmail.com>
2023-12-25 23:41:54 -0300

    theme changer is now a dropdown menu

--- src/components/Actions.svelte
+++ src/components/Actions.svelte
@@ -1,42 +1,51 @@
 <script lang="ts">
   import { onMount } from "svelte";
   export let onClick: () => void;
+  let dropdownText = "";
+  let showDropdown = false;
+
   onMount(() => {
     const theme = localStorage.getItem("theme");
-    switch (theme) {
-      case "ayu":
-        document.body.setAttribute("data-theme", "ayu");
-        break;
-      case "macchiato":
-        document.body.setAttribute("data-theme", "macchiato");
-        break;
-      default:
-        document.body.setAttribute("data-theme", "ayu");
-        break;
+    if (theme) {
+      document.body.setAttribute("data-theme", theme);
+      dropdownText = theme;
+    } else {
+      document.body.setAttribute("data-theme", "ayu");
+      dropdownText = "ayu";
     }
   });
 
-  function changeTheme() {
-    const attr = document.body.getAttribute("data-theme");
-    switch (attr) {
-      case "ayu":
-        document.body.setAttribute("data-theme", "macchiato");
-        localStorage.setItem("theme", "macchiato");
-        break;
-      case "macchiato":
-        document.body.setAttribute("data-theme", "ayu");
-        localStorage.setItem("theme", "ayu");
-        break;
-      default:
-        document.body.setAttribute("data-theme", "ayu");
-        localStorage.setItem("theme", "ayu");
-        break;
+  function getChosenTheme(event: MouseEvent) {
+    const button = event.target as HTMLButtonElement;
+    const chosenTheme = button.textContent;
+    if (chosenTheme) {
+      changeTheme(chosenTheme.toLowerCase());
+      dropdownText = chosenTheme;
     }
+  }
+
+  function changeTheme(theme: string) {
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }
+
+  function showHidenDropdown() {
+    showDropdown = !showDropdown;
   }
 </script>
 
 <div class="btn__wrapper">
-  <button on:click={changeTheme} type="button">theme</button>
+  <div class="dropdown">
+    <button class="dropdown__btn" on:click={showHidenDropdown}
+      >{dropdownText}</button
+    >
+    {#if showDropdown}
+      <div class="dropdown__content">
+        <button on:click={getChosenTheme}>Ayu</button>
+        <button on:click={getChosenTheme}>Macchiato</button>
+      </div>
+    {/if}
+  </div>
   <button on:click={onClick} type="button">wallpaper</button>
 </div>
 
@@ -45,21 +54,44 @@
     display: hidden;
     top: 1em;
     right: 1em;
+    align-items: center;
+    justify-content: center;
+    gap: 1em;
   }
+
   .btn__wrapper button {
     background: none;
     display: hidden;
     color: var(--fg);
     margin-left: 0.375rem;
   }
+
   .btn__wrapper button:hover {
     color: var(--purple-color);
     cursor: pointer;
   }
 
-  @media(min-width: 575px){
-    .btn__wrapper{
-      display: block;
+  .dropdown__content {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    margin-top: 0.2em;
+    padding: 1em;
+    gap: 0.5em;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background: var(--crust);
+    border-radius: 1em;
+  }
+
+  .dropdown__content button {
+    width: 100%;
+  }
+
+  @media (min-width: 575px) {
+    .btn__wrapper {
+      display: flex;
       position: absolute;
     }
   }
