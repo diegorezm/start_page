c88592f4135945ac279ed79fb9f14096e6eca67f
diegorezm <dmourarezende@gmail.com>
2024-08-19 14:39:13 -0300

    fixed default background

--- src/components/container-component.vue
+++ src/components/container-component.vue
@@ -3,8 +3,11 @@ import userWallpaper from "@/lib/context/wallpaper-context";
 import { ref, watch } from "vue";
 
 const container = ref<HTMLDivElement | null>(null);
+const wallpaperExists = ref(false);
 
 const updateBackground = (wallpaper: string | null) => {
+  wallpaperExists.value = !!wallpaper;
+  console.log(wallpaperExists.value);
   if (container.value) {
     container.value.style.backgroundImage = wallpaper
       ? `url(${wallpaper})`
@@ -12,14 +15,24 @@ const updateBackground = (wallpaper: string | null) => {
   }
 };
 
-watch(() => userWallpaper.value, (newWallpaper) => {
-  updateBackground(newWallpaper);
-}, { immediate: true });
+watch(
+  () => userWallpaper.value,
+  (newWallpaper) => {
+    updateBackground(newWallpaper);
+  },
+  { immediate: true },
+);
 </script>
 
 <template>
-  <div class="img_container relative flex flex-col items-center justify-center w-full h-screen overflow-hidden" ref="container">
-    <div class="absolute inset-0 bg-black/60 backdrop-blur-md"/>
+  <div
+    class="img_container bg-background relative flex flex-col items-center justify-center w-full h-screen overflow-hidden"
+    ref="container"
+  >
+    <div
+      class="absolute inset-0 bg-black/60 backdrop-blur-sm"
+      v-if="wallpaperExists"
+    />
     <div class="relative z-10 w-full h-full space-y-5 lg:space-y-20">
       <slot />
     </div>
@@ -32,4 +45,3 @@ watch(() => userWallpaper.value, (newWallpaper) => {
   background-size: cover;
 }
 </style>
-
