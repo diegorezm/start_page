57845ded5f8d0f1d0aad8c7e9b435acad223d0b5
diegorezm <dmourarezende@gmail.com>
2024-10-26 10:28:52 -0300

    new themes

--- src/lib/hooks/use-theme.ts
+++ src/lib/hooks/use-theme.ts
@@ -1,4 +1,4 @@
-export const THEMES = ["default", "ayu-dark", "green", "purple"] as const;
+export const THEMES = ["default", "ayu-dark", "green", "purple", "gruvbox-dark", "gruvbox-light-hard"] as const;
 
 export type Theme = (typeof THEMES)[number];
 
